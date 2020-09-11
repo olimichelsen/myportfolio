1a837756fad35e12e57cd2b0dddd2efa6fd876b8
olimichelsen <56107467+olimichelsen@users.noreply.github.com>
2020-09-11 11:15:47 +0200

    Updated website
    Cusotm CSS added and final layout is ready for content to be added.

--- wp-content/themes/optics-child-theme/functions.php
+++ wp-content/themes/optics-child-theme/functions.php
@@ -13,3 +13,19 @@ function my_theme_enqueue_styles() {
         $theme->get('Version') // this only works if you have Version in the style header
     );
 }
+
+/* This code adds the option to add a logo to the optics theme */
+function themename_custom_logo_setup() {
+ $defaults = array(
+ 'height'      => 400,
+ 'width'       => 400,
+ 'flex-height' => true,
+ 'flex-width'  => true,
+ 'header-text' => array( 'site-title', 'site-description' ),
+ );
+ add_theme_support( 'custom-logo', $defaults );
+}
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+if ( function_exists( 'the_custom_logo' ) ) {
+ the_custom_logo();
+}
